fce54f05c72453373c70356ce781f14bafab92cb
AgitaDumpling <agitaliene.berzina@gmail.com>
2024-05-04 19:30:59 +0300

    .
    .

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
-let flippedCards = [];
-let lockBoard = false;
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', () => {
+      flipCard(card);
+  });
+});
 
 function flipCard(card) {
-    if (lockBoard) return;
-    if (flippedCards.length === 2) return;
-
-    card.classList.add('flip');
-    flippedCards.push(card);
-
-    if (flippedCards.length === 2) {
-        lockBoard = true;
-        setTimeout(checkMatch, 1000);
-    }
-}
-
-function checkMatch() {
-    if (flippedCards[0].querySelector('.card-back').innerText === flippedCards[1].querySelector('.card-back').innerText) {
-        disableCards();
-    } else {
-        unflipCards();
-    }
-}
-
-function disableCards() {
-    flippedCards[0].removeEventListener('click', flipCard);
-    flippedCards[1].removeEventListener('click', flipCard);
-
-    resetBoard();
-}
-
-function unflipCards() {
-    setTimeout(() => {
-        flippedCards[0].classList.remove('flip');
-        flippedCards[1].classList.remove('flip');
-
-        resetBoard();
-    }, 1000);
-}
-
-function resetBoard() {
-    flippedCards = [];
-    lockBoard = false;
+  card.querySelector('.card-inner').classList.toggle('flipped');
 }
