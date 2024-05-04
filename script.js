f83c97ab49bcb29701869389ff3d25823f64398f
AgitaDumpling <agitaliene.berzina@gmail.com>
2024-05-04 19:57:06 +0300

    .
    .

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', () => {
-      flipCard(card);
-  });
-});
 
 function flipCard(card) {
   card.querySelector('.card-inner').classList.toggle('flipped');
